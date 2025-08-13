ffc9ecf301299971cc2127d475a952e0f5f59591
John Doak <johnsiilver@gmail.com>
2025-08-12 22:23:07 -0700

    Fix bug with CompareAndSwap

--- concurrency/sync/shardedmaps.go
+++ concurrency/sync/shardedmaps.go
@@ -2,6 +2,7 @@ package sync
 
 import (
 	"iter"
+	"sync"
 
 	"github.com/gostdlib/base/concurrency/sync/internal/shardmap"
 )
@@ -16,6 +17,7 @@ type ShardedMap[K comparable, V any] struct {
 	// IsEqual is a function that is used to compare two values for equality. This is not required
 	// unless using CompareAndSwap or CompareAndDelete.
 	IsEqual func(a, b V) bool
+	once    sync.Once
 	sm      shardmap.Map[K, V]
 }
 
@@ -39,6 +41,11 @@ func (s *ShardedMap[K, V]) Set(k K, v V) (prev V, ok bool) {
 // If the key does not exist but the old value is the zero value for the type, this will create the value
 // and set it to new. This will return true if the value was set, false otherwise.
 func (s *ShardedMap[K, V]) CompareAndSwap(k K, old, new V) (swapped bool) {
+	s.once.Do(func() {
+		if s.sm.IsEqual == nil {
+			s.sm.IsEqual = s.IsEqual
+		}
+	})
 	return s.sm.CompareAndSwap(k, old, new)
 }
 
@@ -51,6 +58,11 @@ func (s *ShardedMap[K, V]) Del(k K) (prev V, ok bool) {
 // CompareAndDelete deletes the value for the given key if the current value is equal to the old value.
 // If the key does not exist, this returns true.
 func (s *ShardedMap[K, V]) CompareAndDelete(k K, old V) (deleted bool) {
+	s.once.Do(func() {
+		if s.sm.IsEqual == nil {
+			s.sm.IsEqual = s.IsEqual
+		}
+	})
 	return s.sm.CompareAndDelete(k, old)
 }
 
